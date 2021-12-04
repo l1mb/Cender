7371b3b740dd596d3343becfd45e10ec315622bf
Yasper Mag <sergeev.s1012@gmail.com>
2021-12-04 11:22:31 -0800

    add aop logger

---
+++ src/main/java/cender/shop/BL/Utilities/logger/aop/LogAnnotation.java
@@ -0,0 +1,11 @@
+package cender.shop.BL.Utilities.logger.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogAnnotation {
+}
